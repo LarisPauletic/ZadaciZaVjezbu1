7fe35aa754343dafc1c59dba4d71741e7044c563
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-12-11 16:39:45 +0100

    Add files via upload

---
+++ ZadaciZaVjezbu1/ZadaciZaVjezbu1/KlasaC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu1
+{
+    class KlasaC
+    {
+        public int Zbroj(int[] upis)
+        {
+            int sum = 0;
+            for (int i = 0; i < upis.Length; i++)
+                sum += upis[i];
+            return sum;
+        }
+        public double Prosjek(int[] upis)
+        {
+            int sum = 0;
+            for (int i = 0; i < upis.Length; i++)
+                sum += upis[i];
+            return (double)sum / upis.Length;
+        }
+    }
+}
---
+++ ZadaciZaVjezbu1/ZadaciZaVjezbu1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] upis = new int[5];
+
+            Console.WriteLine("Upišite 5 cijelih brojeva:");
+            for (int i = 0; i < 5; i++)
+            {
+                string s = Console.ReadLine();
+                upis[i] = Convert.ToInt32(s);
+            }
+            KlasaC x = new KlasaC();
+            Console.Write("Zbroj upisanih brojeva je: ");
+            int zbroj = x.Zbroj(upis);
+            Console.WriteLine(zbroj);
+
+            Console.Write("Prosjek upisanih brojeva je:");
+            double prosjek = x.Prosjek(upis);
+            
+            Console.WriteLine(prosjek);
+
+            Console.ReadKey();
+        }
+    }
+}
